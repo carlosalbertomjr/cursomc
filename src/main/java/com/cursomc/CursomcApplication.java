a5801946545c866d90c0f9d4134020c09c783ff5
Carlos Alberto <carlosalbertomjr@gmail.com>
2019-08-19 19:58:48 -0300

    Estado e Cidade

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -1,7 +1,6 @@
 package com.cursomc;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.domain.Cidade;
+import com.cursomc.domain.Estado;
 import com.cursomc.domain.Produto;
 import com.cursomc.repository.CategoriaRepository;
+import com.cursomc.repository.CidadeRepository;
+import com.cursomc.repository.EstadoRepository;
 import com.cursomc.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	CategoriaRepository repoCategoria;
 	@Autowired
 	ProdutoRepository repoProduto;
+	@Autowired
+	EstadoRepository repoEstado;
+	@Autowired
+	CidadeRepository repoCidade;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,12 +51,18 @@ public class CursomcApplication implements CommandLineRunner {
 		repoCategoria.saveAll(Arrays.asList(cat1, cat2));
 		repoProduto.saveAll(Arrays.asList(p1,p2,p3));
 		
-		Optional<Produto> p4 = repoProduto.findById(1);
-		if (p4 != null) {
-			System.out.println(p4);
-		} else {
-		  System.out.println("not found!");
-		}
+		Estado minasGerais = new Estado(null, "Minas Gerais");
+		Estado estadoSaoPaulo = new Estado(null,"São Paulo");
+		
+		Cidade uberlandia = new Cidade(null, "Uberlância", minasGerais);
+		Cidade saoPaulo = new Cidade(null, "São Paulo", estadoSaoPaulo);
+		Cidade campinas = new Cidade(null, "Campinas", estadoSaoPaulo);
+		
+		repoEstado.saveAll(Arrays.asList(minasGerais, estadoSaoPaulo));
+		repoCidade.saveAll(Arrays.asList(uberlandia, saoPaulo, campinas));
+		
+		minasGerais.getCidades().addAll(Arrays.asList(uberlandia));
+		estadoSaoPaulo.getCidades().addAll(Arrays.asList(saoPaulo, campinas));
 	}
  
 }
---
+++ src/main/java/com/cursomc/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.cursomc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
