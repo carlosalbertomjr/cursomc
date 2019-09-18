6f307e61a4f58abd10ea2841cdc0fc87896af1a4
Carlos Alberto <carlosalbertomjr@gmail.com>
2019-09-18 19:41:57 -0300

    validação customizada email repetido na inclusão de cliente

--- src/main/java/com/cursomc/domain/Cliente.java
+++ src/main/java/com/cursomc/domain/Cliente.java
@@ -8,16 +8,13 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-
-import org.hibernate.validator.constraints.br.CPF;
 
 import com.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -32,7 +29,7 @@ public class Cliente implements Serializable {
 	private Integer id;
 	//@NotEmpty(message = "Nome inválido")
 	private String nome;
-	//@Email
+	@Column(unique = true)
 	private String email;
 	//	@CPF
 	private String cpfOuCnpj;
--- src/main/java/com/cursomc/service/validation/ClientInsertValidator.java
+++ src/main/java/com/cursomc/service/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.repository.ClienteRepository;
 import com.cursomc.resources.exceptions.FieldMessage;
 import com.cursomc.service.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,10 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 		} 
 		if ((TipoCliente.toEnum(objDto.getTipo()) == TipoCliente.PESSOAJURIDICA) && (!BR.isValidCNPJ(objDto.getCpfOuCnpj()))){ 
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
